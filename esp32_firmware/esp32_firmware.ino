0a7e568d7b325575049b36939fda20d634e584c9
anfas <macbook@Anfas-MacBook-Air.local>
2026-07-03 15:40:08 +0530

    Update ESP32 firmware target URL to new local IP 192.168.1.17

--- esp32_firmware/esp32_firmware.ino
+++ esp32_firmware/esp32_firmware.ino
@@ -71,7 +71,7 @@
 // --- CONFIGURATION ---
 const char* ssid = "Taste Buds";             // Replace with your Wi-Fi SSID
 const char* password = "ansarali134";                 // Replace with your Wi-Fi Password
-const char* serverUrl = "https://rfid-attendance-system-ie52.onrender.com/api/scan"; // Replace with your Node.js server IP
+const char* serverUrl = "http://192.168.1.17:5001/api/scan"; // Replace with your Node.js server IP
 
 // --- HARDWARE PIN OUTS ---
 #define RST_PIN     14  // Relocated to GPIO 14 to allow free I2C SCL pin on GPIO 22
